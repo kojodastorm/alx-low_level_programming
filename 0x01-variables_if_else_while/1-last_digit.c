d2c9184d9c00c87392096c761012ae45edadb91a
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-03 19:01:19 -0700

    create a code to extract data 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if (tmp > 5)
 	{
-		printf("The Last digit of %d is %d and is greater than 5\n", n, tmp);
+		printf("Last digit of %d is %d and is greater than 5\n", n, tmp);
 	}
 	else if (tmp == 0)
 	{
-		printf("The Last digit of %d is %d and is 0\n", n, tmp);
+		printf("Last digit of %d is %d and is 0\n", n, tmp);
 	}
 	else if (tmp < 6)
 	{
-		printf("The Last digit of %d is %d and is less than 6 and not 0\n", n, tmp);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, tmp);
 	}
 	return (0);
 }
