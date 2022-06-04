aa44a078a70b1b5b94c374f3bbcc62661bc40311
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-03 18:46:55 -0700

    create a code to extract data

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
  *main - Entry point
@@ -7,5 +9,24 @@
  */
 int main(void)
 {
+	int n;
+	int tmp;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	tmp = n % 10;
+
+	if (tmp > 5)
+	{
+		printf("The Last digit of %d is %d and is greater than 5\n", n, tmp);
+	}
+	else if (tmp = 0)
+	{
+		printf("The Last digit of %d is %d and is 0\n", n, tmp);
+	}
+	else if (tmp < 6) 
+	{
+		printf("The Last digit of %d is %d and is less than 6 and not 0\n", n, tmp);
+	}
 	return (0);
 }
