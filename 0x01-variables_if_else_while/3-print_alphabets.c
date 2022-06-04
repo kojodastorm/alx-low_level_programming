fcbde6fde112d5af21e4c54b11cf1b4c70a554c6
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-03 19:27:23 -0700

    create a code to bring all alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,5 +7,21 @@
  */
 int main(void)
 {
+	char a;
+
+	a = 97;
+	while (a < 97 + 26)
+	{
+		putchar(a);
+		a++;
+	}
+	a = 65;
+	while (a < 65 + 26)
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar(10);
 	return (0);
 }
