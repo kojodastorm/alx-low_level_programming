2d420f044e383da192e09bbec0e0d2eb4ad68d65
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-07 07:43:46 -0700

    create a code to print _putchar on the screen

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include "_putchar.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int _;
+int p;
+int u;
+int t;
+int c;
+int h;
+int a;
+int r;
+
+int _ = 95;
+int p = 112;
+int u = 117;
+int t = 116;
+int c = 99;
+int h = 104;
+int a = 97;
+int r = 114;
+int main(void)
+{
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar(10);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,15 @@
+
+<H1>This is the readme for the 0x02-functions_nested_loops
+<H2> 0-putchar.c
+<H2> 1-alphabet.c
+<H2> 2-print_alphabet_x10.c
+<H2> 3-islower.c
+<H2> 4-isalpha.c
+<H2> 5-sign.c
+<H2> 6-abs.c
+<H2> 7-print_last_digit.c
+<H2> 8-24_hours.c
+<H2> 9-times_table.c
+<H2> 10-add.c
+<H2> 11-print_to_98.c
+
