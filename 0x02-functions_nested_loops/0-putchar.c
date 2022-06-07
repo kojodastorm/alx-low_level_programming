005ff517ea42b6ae9b02eb2ca10e32081b527bb4
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-07 08:06:09 -0700

    create a code to print _putchar on the screen 4

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,35 +1,20 @@
+#include "main.h"
+#include <stdio.h>
 /**
  *main - Entry point
  *
  *Return: Always 0 (Success)
  */
-int _;
-int p;
-int u;
-int t;
-int c;
-int h;
-int a;
-int r;
-
-int _ = 95;
-int p = 112;
-int u = 117;
-int t = 116;
-int c = 99;
-int h = 104;
-int a = 97;
-int r = 114;
 int main(void)
 {
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar(10);
 	return (0);
 }
