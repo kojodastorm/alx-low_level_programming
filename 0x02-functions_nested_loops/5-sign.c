e9ec3b8afe5d801f58617866633d668ef5e80ff8
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-07 10:30:21 -0700

    create a code to print the sign of a number 3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,6 +18,7 @@ int print_sign(int n)
 	}
 	else
 	{
+		_putchar('0');
 		return (0);
 	}
 }
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	int r;
+
+	r = print_sign(0);
+	_putchar(r);
+	_putchar(r + '0');
+	_putchar('\n');
+}
