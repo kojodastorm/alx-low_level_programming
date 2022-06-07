cc504e1d112e8adddd5698710a9ed19b9423c125
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-07 10:08:24 -0700

    create a code to print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- *main - Entry point
- *
- *Return: Always 0 (Success)
+ *print_sign - prints the sign of the input
+ *@n: contains the integer decimal
+ *Return: int (Success)
  */
-int main(void)
+int print_sign(int n)
 {
-	return (0);
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45)
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
 }
