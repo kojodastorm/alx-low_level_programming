3e74b9169168bbcc690893b3ce10317ff1640d12
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-07 12:40:22 -0700

    create a code to print last numbers 3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_last_digit - prints the last digit of the number
  *@n: contains the integer decimal
@@ -6,11 +7,15 @@
  */
 int print_last_digit(int n)
 {
+	if (n < -10000)
+	{
+		n-= 1000;
+	}
 	if (n < 0)
 	{
 		n *= -1;
 	}
-	n = abs(n) % 10;
+	n = n % 10;
 	_putchar(n + 48);
 	return (n);
 }
