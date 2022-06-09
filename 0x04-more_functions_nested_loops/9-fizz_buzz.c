7aa72d257b9766f83577060166b62b1c142eac8a
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-09 12:49:41 -0700

    create a code to print fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - print Fizz Buzz in some places
+ *
+ * Return: int(success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i > 1)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf(" FizzBuzz");
+			}
+			else if (i % 3 == 0)
+			{
+				printf(" Fizz");
+			}
+			else if (i % 5 == 0)
+			{
+				printf(" Buzz");
+			}
+			else
+			{
+				printf(" %d", i);
+			}
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	
+	return (0);
+}
+
