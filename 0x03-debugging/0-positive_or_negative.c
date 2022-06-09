048666b61db81c515bf052b3343254f29877b560
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-08 19:16:19 -0700

    create a code to test a function 2

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	int n;
+	n = i;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+}
