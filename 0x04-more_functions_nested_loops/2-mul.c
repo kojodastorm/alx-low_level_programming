a9b4603bb85fa813a3a6a69ff8c35ece3945438e
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-09 11:21:53 -0700

    create a code to multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers
+ *
+ * @a: decimal integer
+ * @b: decimal integer
+ *
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
