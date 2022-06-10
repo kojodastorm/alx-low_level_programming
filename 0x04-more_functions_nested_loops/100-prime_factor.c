fef31f32019f4b13454960b736ff4aaac9ebcd3a
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-09 17:21:24 -0700

    create a code to find largest prime factor 3

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,17 +12,16 @@ int prime(unsigned long int a);
 
 int main(void)
 {
-	int i = 2;
+	unsigned long int i = 2;
 	unsigned long int num;
 	unsigned long int a;
 
 	a = 612852475143;
 	if (prime(a))
 	{
-		printf("%lu\n", a);
 		return (0);
 	}
-	while (i != 0)
+	while (i <= a / 2)
 	{
 		if (prime(i))
 		{
@@ -69,20 +68,22 @@ int main(void)
 
 int prime(unsigned long int a)
 {
-	if (a > 1 && (a == 2 || a == 3 || a == 5))
+	unsigned long int i;
+
+	if (a > 3)
+	{
+		for (i = 2; i <= a / 2; i++)
+		{
+			if  (a % i == 0)
+			{
+				return (0);
+			}
+		}
+		return (1);
+	}
+	else if (a == 2 || a == 3)
 	{
 		return (1);
 	}
-	else if (a > 9)
-	{
-		if (a % 10 == 1 || a % 10 == 3 || a % 10 == 7 || a % 10 == 9)
-		{
-			return (1);
-		}
-	}
-	else
-	{
-		return (0);
-	}
-	return (0);
+	return(0);
 }
