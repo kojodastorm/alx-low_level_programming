2196381054717d52fba7e425c42f743a7d610638
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-13 20:06:00 -0700

    create a code to change original variable passed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this will change the original value passed to it
+ *@n: points to the variable passed to it.
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
