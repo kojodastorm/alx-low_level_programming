6e148f59b0f772cf7e8d4a6775ac706e74b1f97c
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-13 22:01:08 -0700

    create a code to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - this will reverse a string
+ *@s: points to the address of string passed to it.
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int k;
+	int j = 0;
+	char tmp;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s = s + 1;
+	}
+	i--;
+	k = i;
+
+	while (i >= k / 2)
+	{
+		s = s - 1;
+		tmp = *(s - (i - j));
+		*(s - (i - j)) = *s;
+		*s = tmp;
+		i--;
+		j++;
+	}
+}
