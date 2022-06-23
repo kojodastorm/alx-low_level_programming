45cc3e2ed5c843b041edfe173a616407712d1392
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-06-23 15:18:00 -0700

    create a code to print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_number - print from any multi digit number
+ * @n: number to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i = 1;
+	int j;
+	int state;
+	int tmp;
+
+	if (n < 0)
+	{
+		n *= -1;
+		state = 45;
+	}
+	tmp = n;
+	while (1)
+	{
+		tmp /= 10;
+		if (tmp == 0)
+		{
+			break;
+		}
+		i++;
+	}
+	tmp = i;
+	i = 1;
+	for (j = 1; j < tmp; j++)
+	{
+		i *= 10;
+	}
+	while (i > 0)
+	{
+		if (state == 45)
+		{
+			_putchar('-');
+			state = 0;
+		}
+		_putchar(n / i + 48);
+		n %= i;
+		i /= 10;
+	}
+}
