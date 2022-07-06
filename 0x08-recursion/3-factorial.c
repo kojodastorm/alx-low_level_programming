5aefd764cf078a4adac995f295d78d286883ac1f
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-07-05 19:35:49 -0700

    create a code to calc factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * factorial - outputs factorial
+ *
+ * @n: integer
+ *
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		while (n > 0)
+		{
+			n--;
+			return ((n + 1) * factorial(n));
+		}
+
+	}
+	return (0);
+}
