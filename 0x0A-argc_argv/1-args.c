5e9ea803bc83d6845227afa51b13422548a5fd5a
Kojo Kwakye Cofie <kojokwakye@gmail.com>
2022-07-10 19:41:54 -0700

    create a code to print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
